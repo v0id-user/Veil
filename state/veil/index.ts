616890f4a51367e02e12fd85a706bcd9f8f25ee6
#V0ID <hey@v0id.me>
2025-01-22 02:26:54 +0300

    refactor: remove RoomState interface and Zustand store for room management

--- state/veil/index.ts
+++ state/veil/index.ts
@@ -1,8 +1,10 @@
-import RoomState from "@/interface/state/room";
+import RoomState from "@/interface/room";
 import { create } from 'zustand'
 
 // TODO: Make Two states one for the rooms sidebar and one for the room interface
 // TODO: Add more states like if the other end is typing or not or if it connected and secured or not
+
+// ! Single source of truth for the all the messages state as everything about this app is about the messages
 
 const useRoomStore = create<RoomState>((set, get) => {
     return {
